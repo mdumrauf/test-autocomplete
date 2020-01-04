813b3f5d5365d180459b7e5be9b088fbd64b985b
Matias Dumrauf <matias.dumrauf@southworks.com>
2020-01-04 18:53:27 -0300

    feat(app): use Class-Component for AutcompleteApp

--- src/AutocompleteApp.js
+++ src/AutocompleteApp.js
@@ -1,14 +1,19 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './AutocompleteApp.css';
 
-export default function AutocompleteApp() {
-  return (
-    <form className="autocomplete-app">
-      <input type="text" autoFocus />
-      <ul className="autocomplete-suggestions">
-        <li>Example suggestion.</li>
-      </ul>
-    </form>
-  );
+export class AutocompleteApp extends Component {
+  render() {
+    return (
+      <form className="autocomplete-app">
+        <input
+          type="text"
+          autoFocus />
+        <ul className="autocomplete-suggestions">
+          <li>Example suggestion.</li>
+        </ul>
+      </form>
+    );
+  }
 }
+
+export default AutocompleteApp;
