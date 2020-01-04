2e57fe754759c823fa0dafe56568cc6d30abcae8
Matias Dumrauf <matias.dumrauf@southworks.com>
2020-01-04 20:02:38 -0300

    feat(app): add onChange handler and saved input value

--- src/AutocompleteApp.js
+++ src/AutocompleteApp.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import './AutocompleteApp.css';
 
 export class AutocompleteApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: ''
+    };
+  }
+
+  onChange = (event) => {
+    const userInput = event.currentTarget.value;
+
+    this.setState({
+      userInput
+    });
+  };
+
   render() {
+    const {userInput, onChange} = this;
+
     return (
       <form className="autocomplete-app">
         <input
           type="text"
-          autoFocus />
+          autoFocus
+          onChange={onChange}
+          value={userInput} />
         <ul className="autocomplete-suggestions">
           <li>Example suggestion.</li>
         </ul>
