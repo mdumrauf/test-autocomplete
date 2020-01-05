cf6e358e658969c5dfc97e75935fc64f88a89664
Matias Dumrauf <matias.dumrauf@southworks.com>
2020-01-05 18:38:32 -0300

    feat(app): implement up/down arrow navigation and enter key selection

--- src/AutocompleteApp.js
+++ src/AutocompleteApp.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import {isEmpty, map} from 'lodash';
 import './AutocompleteApp.css';
 
+const ENTER_KEY = 13;
+const UP_KEY = 38;
+const DOWN_KEY = 40;
+
 export class AutocompleteApp extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       books: [],
+      currentBookIndex: 0,
       userInput: ''
     };
   }
@@ -24,20 +29,55 @@ export class AutocompleteApp extends Component {
     });
   };
 
-  onClick = e => {
+  selectBook(bookId, bookTitle) {
     this.setState({
       books: [],
-      userInput: e.currentTarget.innerText
+      userInput: bookTitle
     });
-    this.props.history.push(`/books/${e.target.id}`);
+    this.props.history.push(`/books/${bookId}`);
+  }
+
+  onKeyDown = e => {
+    const { currentBookIndex, books } = this.state;
+
+    switch (e.keyCode) {
+      case ENTER_KEY:
+        const selectedBook = books[currentBookIndex];
+        this.setState({
+          currentBookIndex: 0,
+        });
+        this.selectBook(selectedBook.id, selectedBook.title);
+        break;
+      case UP_KEY:
+        if (currentBookIndex === 0) {
+          return;
+        }
+        this.setState({ currentBookIndex: currentBookIndex - 1 });
+        break;
+      case DOWN_KEY:
+        if (currentBookIndex - 1 === books.length) {
+          return;
+        }
+
+        this.setState({ currentBookIndex: currentBookIndex + 1 });
+        break;
+      default:
+        return;
+    }
+  };
+
+  onClick = e => {
+    this.selectBook(e.target.id, e.currentTarget.innerText);
   };
 
   render() {
     const {
       onChange,
       onClick,
+      onKeyDown,
       state: {
         books,
+        currentBookIndex,
         userInput
       }
     } = this;
@@ -46,10 +86,16 @@ export class AutocompleteApp extends Component {
 
     if (!isEmpty(userInput) && !isEmpty(books)) {
       suggestionsList = (
-        map(books, book => {
+        map(books, (book, pos) => {
+          let className;
+
+          if (pos === currentBookIndex) {
+            className = "selected";
+          }
           return (
             <li
               id={book.id}
+              className={className}
               key={book.id}
               onClick={onClick}>
               {book.title}
@@ -65,6 +111,7 @@ export class AutocompleteApp extends Component {
           type="text"
           autoFocus
           onChange={onChange}
+          onKeyDown={onKeyDown}
           value={userInput} />
         <ul className="autocomplete-suggestions">
           {suggestionsList}
