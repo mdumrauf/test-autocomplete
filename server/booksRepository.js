30630a82127121562f72100d5c8a33a58c274ade
Matias Dumrauf <matias.dumrauf@southworks.com>
2020-01-05 14:44:54 -0300

    feat(server): implement BooksRepository.findById()

--- server/booksRepository.js
+++ server/booksRepository.js
@@ -1,6 +1,11 @@
+const {find} = require('lodash');
 const {findAll} = require('./books');
 
 class BooksRepository {
+
+  static findById(id) {
+    return find(this.findAll().data, (book) => book.id === id);
+  }
 
   static findAll() {
     return findAll();
