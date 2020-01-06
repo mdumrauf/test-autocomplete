ea04fe8e8bd6829ed9f3964d3ad0c1e35b49703a
Matias Dumrauf <matias.dumrauf@southworks.com>
2020-01-05 23:18:05 -0300

    fix(test): fix app test

--- src/AutocompleteApp.test.js
+++ src/AutocompleteApp.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import AutocompleteApp from './AutocompleteApp';
 
+const router = (
+  <Router>
+    <AutocompleteApp/>
+  </Router>
+)
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AutocompleteApp />, div);
+  ReactDOM.render(router, div);
   ReactDOM.unmountComponentAtNode(div);
 });
